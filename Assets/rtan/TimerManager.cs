8b7e38c740b4f46d5dd005daf7338b7ed6b4ac86
ChocoMucho <vkclssha14@gmail.com>
2023-12-26 20:05:31 +0900

    feature: 추가 시간 연출
    feature: 추가 시간 연출

--- Assets/rtan/TimerManager.cs
+++ Assets/rtan/TimerManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class TimerManager : MonoBehaviour
 {
     public static TimerManager instance;
+    public GameObject plusTimeText;
 
     public float elapsedTime { get; private set; }
 
@@ -26,6 +29,9 @@ public class TimerManager : MonoBehaviour
     public void IncreaseTime(float plusTime) 
     {
         elapsedTime += plusTime;
+        GameObject newPlusTime = Instantiate(plusTimeText);
+        newPlusTime.transform.parent = GameObject.Find("Canvas").transform;
+        plusTimeText.GetComponent<Text>().text = "+" + plusTime.ToString();
     }
 
     public void DecreaseTime(float minusTime) 
