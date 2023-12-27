c4bf6aaaadd790424a9fffedb9708fcd7a5d2b05
박지훈 <154571034+fgonlygle00@users.noreply.github.com>
2023-12-27 10:50:12 +0900

    남은 시간 표시 오류 수정

--- Assets/rtan/GameManager.cs
+++ Assets/rtan/GameManager.cs
@@ -204,7 +204,7 @@ public class GameManager : MonoBehaviour
 
     void endgame()//정상적으로 다 맞추고 종료하면 결과 점수 표시
     {
-        float finalTime = 90f - time;
+        float finalTime = 90f - TimerManager.instance.elapsedTime;
         string finalT = string.Format("{0:N2}", finalTime);
 
         if (finalTime < 0)
